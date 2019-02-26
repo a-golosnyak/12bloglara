efabb92ca3b4f411bec3e39467a0c33e90e6c01c
a-golosnyak <andrey_g.pt@mail.ru>
2019-02-26 23:07:59 +0200

    Factories added. Appears ability to fill database with a test data.

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,18 +2,21 @@
 
 use Faker\Generator as Faker;
 use App\User;
+use App\Category;
 
 $factory->define(App\Post::class, function (Faker $faker) {
     return [
         'user_id' => function(){
             return factory('App\User')->create()->id;
         },
-        'category_id' => function(){
-            return factory('App\Category')->create()->id();
-        },
-        'title' => $faker->words,
-        'intro' => $faker->sentence,
+ /*       'category_id' => function(){
+            return factory('App\Category')->create()->id;
+        },*/
+        'category_id' => rand(1, 5),
+        'title' => $faker->sentence,
+        'intro' => $faker->paragraph(10),
         'img' => "/images/posts/2018-11-19_143430_Kak_s_79.jpg",
-        'body' => $faker->paragraph,
+        'body' => $faker->paragraph(50),
     ];
+
 });
