92b5fcb750ab6bb7eda766092042cb9ff9e50be6
a-golosnyak <andrey_g.pt@mail.ru>
2019-03-11 23:36:46 +0200

    Home changes added.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,7 @@ use App\User;
 use App\Comment;
 use Illuminate\Support\Facades\Auth;
 
-class PostController extends Controller 
+class PostController extends Controller
 {
     /**
      * Create a new controller instance.
@@ -39,29 +39,33 @@ class PostController extends Controller
         else
             $posts = Post::where('category_id', $id)->orderBy('created_at', 'desc')->paginate(7);
 
-        return view('home', ['posts' => $posts]);                             
+//        return($posts );            // Debug
+
+        return view('home', ['posts' => $posts]);
     }
 
     public function getPostsOfUser($id=0)
     {
         $posts = Post::where('user_id', $id)->orderBy('id', 'desc')->paginate(7);
-        
-        return view('home', ['posts' => $posts]);                              
+
+        return view('home', ['posts' => $posts]);
     }
 
     public function getPost($id)
     {
         $posts = Post::where('id', $id)->get();
         $comments = Comment::where('post_id', $id)->where('parent_comment_id', 0)->orderBy('created_at', 'desc')->get();
-        
+
         $nested_comments = null;
         foreach ($comments as $comment) {
             $nested_comments[$comment->id][] = Comment::where('parent_comment_id', $comment->id)->get();
         }
 
+//        dd($posts );            // Debug
+
         return view('article', [    'post' => $posts[0],
                                     'comments' => $comments,
-                                    'nested_comments' => $nested_comments]);    
+                                    'nested_comments' => $nested_comments]);
     }
 
     public function addPost()
@@ -75,8 +79,8 @@ class PostController extends Controller
     }
 
     public function editPost($id)
-    { 
-        $posts = Post::where('id', $id)->get();   
+    {
+        $posts = Post::where('id', $id)->get();
 
         if(Auth::user()->email == $posts[0]->user->email)
         {
@@ -94,7 +98,7 @@ class PostController extends Controller
     }
 
     public function updatePost(Request $request)
-    {       
+    {
         $this->validate($request, [
             'id'=>'required',
             'author' => 'required',
@@ -102,28 +106,28 @@ class PostController extends Controller
             'post_title' => 'required|max:220',
             'post_intro' => 'required|max:1200',
             'post_image' => 'max:10000',
-            'post_body' => 'required'            
+            'post_body' => 'required'
         ]);
 
         $post = new Post();
         $post->id = $request->input('id');
         $post->user_id = $request->input('author');
         $post->category_id = $request->input('category');
-        $post->title = $request->input('post_title');   
-        $post->intro = $request->input('post_intro'); 
+        $post->title = $request->input('post_title');
+        $post->intro = $request->input('post_intro');
         $post->body  = $request->input('post_body');
 
-        $posts = Post::where('id', $post->id)->get(); 
+        $posts = Post::where('id', $post->id)->get();
 
         if(Auth::user()->email == $posts[0]->user->email)
         {
-            if ($request->hasFile('post_image')) 
+            if ($request->hasFile('post_image'))
             {
                 $path = $request->post_image->path();
                 $art_title_trnslt = self::translit($request->input('post_title'));
                 $tmp = substr($art_title_trnslt, 0, 5);                             // substr делает ошибку с кирилическим текстом.
-                $imageName = '../images/posts/'. 
-                            date("Y-m-d_His") .'_'. 
+                $imageName = '../images/posts/'.
+                            date("Y-m-d_His") .'_'.
                             $tmp .'_'. mt_rand(0, 1000) . '.' .
                             $request->post_image->getClientOriginalExtension();
                 $request->post_image->move(public_path('images/posts'), $imageName);
@@ -134,7 +138,7 @@ class PostController extends Controller
                         'category_id'   =>$post->category_id,
                         'title'         =>$post->title,
                         'intro'         =>$post->intro,
-                        'img'           =>$post->img, 
+                        'img'           =>$post->img,
                         'body'          =>$post->body]);
 
                 return redirect("/$post->id")->with('status', 'Пост обновлен.');
@@ -173,22 +177,22 @@ class PostController extends Controller
     }
 
     public function submit(Request $request)
-    {       
+    {
         $this->validate($request, [
             'author' => 'required',
             'category' => 'required',
             'post_title' => 'required|max:220',
             'post_intro' => 'required|max:1200',
             'post_image' => 'required',
-            'post_body' => 'required'            
+            'post_body' => 'required'
         ]);
 
         if ($request->file('post_image')->isValid()) {
             $path = $request->post_image->path();
             $art_title_trnslt = self::translit($request->input('post_title'));
             $tmp = substr($art_title_trnslt, 0, 5);                             // substr делает ошибку с кирилическим текстом.
-            $imageName = '../images/posts/'. 
-                            date("Y-m-d_His") .'_'. 
+            $imageName = '../images/posts/'.
+                            date("Y-m-d_His") .'_'.
                             $tmp .'_'. mt_rand(0, 1000) . '.' .
                             $request->post_image->getClientOriginalExtension();
             $request->post_image->move(public_path('images/posts'), $imageName);
@@ -202,15 +206,15 @@ class PostController extends Controller
         $post->category_id = $request->input('category');
         $post->title = $request->input('post_title');
         $post->img = $imageName;
-        $post->intro = $request->input('post_intro'); 
-        $post->body  = $request->input('post_body'); 
+        $post->intro = $request->input('post_intro');
+        $post->body  = $request->input('post_body');
         $post->save();
 
         return redirect("/")->with('status', 'Пост получен и готовится к публикации.');
     }
 
 
-    function translit($str) 
+    function translit($str)
     {
         $rus = array('А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', ' ');
         $lat = array('A', 'B', 'V', 'G', 'D', 'E', 'E', 'Gh', 'Z', 'I', 'Y', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H', 'C', 'Ch', 'Sh', 'Sch', 'Y', 'Y', 'Y', 'E', 'Yu', 'Ya', 'a', 'b', 'v', 'g', 'd', 'e', 'e', 'gh', 'z', 'i', 'y', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'ch', 'sh', 'sch', 'y', 'y', 'y', 'e', 'yu', 'ya', '_');
@@ -226,17 +230,17 @@ class PostController extends Controller
 
 
 
-/*        
+/*
         echo "<pre>";
         foreach ($comments as $comment) {
-            foreach($nested_comments[$comment->id] as $nested_comment){            
+            foreach($nested_comments[$comment->id] as $nested_comment){
                 foreach ($nested_comment as $var) {
                     echo $var->id;
                     echo "<br>" ;
                 }
             }
-        }   
-        echo "</pre>";  
+        }
+        echo "</pre>";
 
 
 
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,6 +28,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+//        if($this->app->isLocal() )
+        {
+            $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+        }
     }
 }
